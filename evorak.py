8e5dc15c2c4bdad4a3de1c45279db8ef2ff01603
Ben Thompson <benjamin.thompson@fkie.fraunhofer.de>
2019-05-29 17:03:01 +0200

    csv formatting

--- evorak.py
+++ evorak.py
@@ -54,16 +54,25 @@ class dictionary():
 
 
     # word list source http://crr.ugent.be/programs-data/subtitle-frequencies
+    # csv files should be formatted with commas as field delimiters and 
+    # double quotes as text delimiters
     def load_file():
         with open ("subtlex-it.csv","r") as file:
+            # Skip the header if the file has one
             has_header = csv.Sniffer().has_header(file.read(1024))
             file.seek(0)  # Rewind.
-            reader = csv.reader(file)
+            reader = csv.reader(file, delimiter=',', quotechar='"' )
             if has_header:
                 next(reader)  # Skip header row.
-            myreader = csv.reader(csvfile, delimiter=',')
+
+
             count = 0
             for row in myreader:
+                word = row[1]
+                freq = float(row[2])
+                
+                if any(char.isdigit() for char in row[1])
+                    continue
                 if (len(row[1]) > 0):
                     wordList.append(row[1])
                     for letter in row[1]:
